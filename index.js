dad934f9f2b2d445a39d65507cee1f4e66a16533
s133p <darkchild50@gmail.com>
2017-10-18 14:35:26 -0700

    Konami code fun

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ Mousetrap.bind('y', function() { window.location = "https://www.youtube.com/" })
 Mousetrap.bind('f', function() { window.location = "https://www.facebook.com/" });
 Mousetrap.bind('i', function() { window.location = "https://inbox.google.com/?pli=1" });
 Mousetrap.bind('n', function() { window.location = "https://www.netflix.com/browse" });
+
+Mousetrap.bind('up up down down left right left right b a', function() {
+    var test = document.getElementsByTagName("a")
+    for (i in test){
+        if(test[i] && test[i] !== undefined && test[i].classList != undefined)
+            test[i].classList.add("hover")
+    }
+
+    setTimeout(() => {
+        var test = document.getElementsByTagName("a")
+        for (i in test){
+            if(test[i] && test[i] !== undefined && test[i].classList != undefined)
+                test[i].classList.remove("hover")
+        }
+    }, 1.25*1000)
+});
+
+
